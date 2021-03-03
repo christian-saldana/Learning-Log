48dd9fc8c7833b03dd2be81cc2bb009989b0343f
ChristianSB24 <christiansaldana10@gmail.com>
2021-03-02 21:36:35 -0600

    Added Users to model and updated base title

--- learning_posts/models.py
+++ learning_posts/models.py
@@ -2,9 +2,12 @@ from django.conf import settings
 from django.db.models import Q
 from django.db import models
 
+User = settings.AUTH_USER_MODEL
+
 
 class Topic(models.Model):
     """A topic the user is learning about."""
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     text = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)
 
