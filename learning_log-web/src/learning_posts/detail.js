3c9985af8e560b9d471afbfef0d51210335c2d17
ChristianSB24 <christiansaldana10@gmail.com>
2021-03-28 23:41:53 -0500

    Updated detail file and index

--- learning_log-web/src/learning_posts/detail.js
+++ learning_log-web/src/learning_posts/detail.js
@@ -5,7 +5,18 @@ import React from 'react'
 export function Topic(props) {
     const {topic} = props
     const className = props.className ? props.className : 'col-10 mx-auto col-md-6'
+    const path = window.location.pathname
+    const match = path.match(/(?<topicid>\d+)/ )
+    const urlTopicId = match ? match.groups.topicid : -1
+    const isDetail = `${topic.id}` === `${urlTopicId}`
+    const handleLink = (event) => {
+      event.preventDefault()
+      window.location.href = `/${topic.id}`
+    }
     return <div className={className}>
-      <p>{topic.id} - {topic.text}</p>
+      <div>
+        <p>{topic.id} - {topic.text}</p>
+      </div>
+      {isDetail === true ? null : <button className='btn btn-outline-primary' onClick={handleLink}>View</button>}
     </div>
   }
