3010082734835cd5a0db2fba029179ffd910cdfe
ChristianSB24 <christiansaldana10@gmail.com>
2021-04-09 23:42:23 -0500

    Added entries to detail view

--- learning_log-web/src/learning_posts/detail.js
+++ learning_log-web/src/learning_posts/detail.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import './styles.css'
 
 export function Topic(props) {
     const {topic} = props
-    const className = props.className ? props.className : 'col-10 mx-auto col-md-6'
+    const className = props.className ? props.className : 'col-50'
     const path = window.location.pathname
     const match = path.match(/(?<topicid>\d+)/ )
     const urlTopicId = match ? match.groups.topicid : -1
@@ -14,7 +15,8 @@ export function Topic(props) {
     }
     return <div className={className}>
       <div>
-        <p>{topic.id} - {topic.post_topic}</p>
+        <p>{topic.id} - {topic.post_topic}:</p>
+        <p className='entry'>{topic.entries}</p>
       </div>
       {isDetail === true ? null : <button className='btn btn-outline-primary' onClick={handleLink}>View</button>}
     </div>
